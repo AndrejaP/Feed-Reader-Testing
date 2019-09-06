d416a035142f43d053a70b2f04650cc56d7af2ea
Andreja <andreja.prpic@gmail.com>
2019-09-06 13:20:14 +0200

    feat: second test
    Check if each feed has a URL defined and that the URL is not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,6 +44,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have a name defined', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            })
+        })
+
     });
 
 
